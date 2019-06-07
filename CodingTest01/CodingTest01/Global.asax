45131e892ff2eeb15ee179c45d34b726b8a8c829
vitamin19 <51465047+vitamin19@users.noreply.github.com>
2019-06-07 11:31:23 -0700

    done

---
+++ CodingTest01/CodingTest01/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="CodingTest01.Global" Language="C#" %>
---
+++ CodingTest01/CodingTest01/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CodingTest01.Startup))]
+namespace CodingTest01
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
